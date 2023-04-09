b5d17a3868e63b218382293f130c181c9a0f39dd
Ivan Shchipin <ivan.shchipin@gmail.com>
2023-04-09 16:05:04 +0300

    vite default port set

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import * as path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +8,7 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-});
+  server: {
+    port: 3000,
+  },
+})
